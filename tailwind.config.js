3ca96e3d6a2fc917e3db46aa9dd32b0b46bd0363
OJailson17 <JaylsonO17@gmail.com>
2023-04-07 21:25:53 -0300

    chore: setup taiÃlwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			colors: {
+				primary: '#1D539E',
+				secondary: '#FFCC29',
+				'primary-text': '#1E2338',
+				'secondary-text': '#828799',
+			},
+		},
+	},
+	plugins: [],
+};
